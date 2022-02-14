fccf191212279c4dde92b14abaa529e4f4d33af6
tcg-pc1 <lindokuhlea38@gmail.com>
2022-02-14 08:23:22 +0200

    new modified files

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 // Load google charts
-google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
+// google.charts.load('current', {'packages':['corechart']});
+// google.charts.setOnLoadCallback(drawChart);
 
-// Draw the chart and set the chart values
-function drawChart() {
-  var data = google.visualization.arrayToDataTable([
-  ['language', 'percentage'],
-  ['HTML', 90],
-  ['CSS', 85],
-  ['JAVASCRIPT', 60],
-  ['REACT.JS', 20],
-  ['BOOTSTRAP', 80],
-  ['SASS', 85],
-  ['JQUERY', 56]
-]);
+// // Draw the chart and set the chart values
+// function drawChart() {
+//   var data = google.visualization.arrayToDataTable([
+//   ['language', 'percentage'],
+//   ['HTML', 90],
+//   ['CSS', 85],
+//   ['JAVASCRIPT', 60],
+//   ['REACT.JS', 20],
+//   ['BOOTSTRAP', 80],
+//   ['SASS', 85],
+//   ['JQUERY', 56]
+// ]);
 
-  // Optional; add a title and set the width and height of the chart
-  var options = { 'title':'My Skills', 'width':700, 'height':780};
+//   // Optional; add a title and set the width and height of the chart
+//   var options = { 'title':'My Skills', 'width':700, 'height':780};
 
-  // Display the chart inside the <div> element with id="piechart"
-  var chart = new google.visualization.PieChart(document.getElementById('skills'));
-  chart.draw(data, options);
-}
+//   // Display the chart inside the <div> element with id="piechart"
+//   var chart = new google.visualization.PieChart(document.getElementById('skills'));
+//   chart.draw(data, options);
+// }
